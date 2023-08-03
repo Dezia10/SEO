3a181448fc0ecd4edf56efb9df3d81c3bfd0e211
Esther Asuquo <estheroasuquo@gmail.com>
2023-08-03 00:45:14 +0000

    Still working on it

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 // 1. get the api url to use
 // 2. use async await and fetch to parse url
 // 3. write code for different options in dropdown menu
+//4. Please add a search() that finds the movie at the search bar
