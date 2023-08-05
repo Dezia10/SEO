acf7673527ad5a1d48b62070582383f53696fb70
Desmond Ababio <dna35@cornell.edu>
2023-08-05 15:49:18 -0400

    finished quotes

--- reactProj/seo/src/Pages/Home.jsx
+++ reactProj/seo/src/Pages/Home.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Quotes from "../UI/Quotes";
 
 const Home = () => {
+  const [quotes, setQuote] = useState([]);
+  useEffect(() => {
+    const fetchMembers = async () => {
+      try {
+        const quotesGenerated = await fetch("https://dummyjson.com/quotes");
+        // );
+        const json = await quotesGenerated.json();
+
+        if (quotesGenerated.ok) {
+          setQuote([...json.quotes]);
+        }
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    fetchMembers();
+  }, []);
   return (
     <div>
-      <Quotes />
+      <div>
+        <input type="text"></input>
+      </div>
+      <Quotes quotes={quotes} />
     </div>
   );
 };
--- reactProj/seo/src/UI/Quotes.jsx
+++ reactProj/seo/src/UI/Quotes.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { v4 as uuid } from "uuid";
+import "./Quotes.css";
 
-const Quotes = () => {
-  return <div>Quotes</div>;
+const Quotes = ({ quotes }) => {
+  return (
+    <div className="quotes-wrapper">
+      {quotes.map((item) => (
+        <div key={uuid()} className="quotes">
+          <li>{item.quote}</li>
+          <p>{item.author}</p>
+          <button>Add to favorites</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Quotes;
